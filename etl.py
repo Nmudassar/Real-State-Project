bdf41e30237a94fd24d8fef33d6f10594bd72008
NadiaMudassar <nadia.syed77@gmail.com>
2025-10-22 20:53:48 +0100

    import libraries and define function

--- etl.py
+++ etl.py
@@ -1,14 +1,22 @@
 import requests
-# importan json
+#import json
 from src.config import BASE_URL, API_KEY
 
 
 
 def extract_properties(city='San Antonio', state='TX'):
-    headers = {"accept": "application/json",
-               X-Api-Key: "RENCAST_API_KEY"
-               }
-    params = {
-        "city": city,
-        "state": state,
+    headers = {
+        "accept": "application/json",
+        "X-Api-Key": API_KEY
     }
+    params = {
+             "city": city,
+             "state": state,
+    }
+    print(f"fetching properties data for {city}, {state}")
+    response = requests.get(BASE_URL, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        
--- src/config.py
+++ src/config.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
-
+# dotenv is python laibaray to load the environment variables from .env file
 load_dotenv()
 
-API_KEY = os.getenv('RENTCAST_API_KEY')
-
+API_KEY = os.getenv("RENTCAST_API_KEY")
 BASE_URL = "https://api.rentcast.io/v1/properties"
